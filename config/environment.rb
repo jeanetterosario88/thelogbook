a666f43303e06d977dd5b40210bdd4276237fd27
jeanetterosario88 <jeanetterosario88@gmail.com>
2020-07-18 14:04:27 -0400

    environment update

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,19 @@
-require 'bundler'
+# require 'bundler'
 
-Bundler.require
+# Bundler.require
 
-ActiveRecord::Base.establish_connection(
-    :adapter => "sqlite3",
-    :database => "db/development.sqlite"
-)
+# ActiveRecord::Base.establish_connection(
+#     :adapter => "sqlite3",
+#     :database => "db/development.sqlite"
+# )
+
+# require_all 'app'
+
+ENV['SINATRA_ENV'] ||= "development"
+
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+ActiveRecord::Base.establish_connection(ENV['SINATRA_ENV'].to_sym)
 
 require_all 'app'

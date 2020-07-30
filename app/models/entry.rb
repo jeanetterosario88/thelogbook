a9051df8586f535d829347d2335412fad0ec0173
jeanetterosario88 <jeanetterosario88@gmail.com>
2020-07-30 19:45:50 -0400

    adding validations in Models

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,19 @@
 class Entry < ActiveRecord::Base
     belongs_to :user
 
+    validates :category
+        presence: true,
+
+    validates :date
+        presence: true,
+
+    validates :rating
+        presence: true, 
+
+    validates :content
+        presence: true, 
+
+    validates :contact
+        presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,16 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :entries
 
+    validates :username,
+          presence: true,
+          length: { minimum: 4, maximum: 16 },
+          uniqueness: true
+
+    validates :email 
+        presence: true
+
+    validates :password, 
+          presence: true
+
 
 end
